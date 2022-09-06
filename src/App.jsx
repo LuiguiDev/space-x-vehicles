d6d5ca1d25fdac9d89cf9ff2edf8b55da3e3709c
Luis Rodríguez <lfrz.arc@gmail.com>
2022-09-06 12:19:26 -0500

    Working with sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
-import './App.css';
+import './App.scss';
 import Spacecraft from './components/Spacecraft.jsx'
-
-var vehicles = [
-  {
-    name:'Falcon 9',
-    image:'falcon-9',
-    height:'70 m',
-    diameter:'3.7 m',
-    mass:'549,054 kg',
-    payload:'22,800 kg'
-  },
-  {
-    name:'Falcon heavy',
-    image:'falcon-heavy',
-    height:'70 m',
-    diameter:'3.7m',
-    mass:'1,420,788 Kg',
-    payload:'63,000 Kg',
-  },
-  {
-    name:'Starship SN15',
-    image:'starship',
-    height:'120 m',
-    diameter:'9 m',
-    mass:'',
-    payload:'100 t',
-  }
-]
+import Vehicles from './obj.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Space X vehicles</h1>
       <div className='vehicles'>
-      <Spacecraft {...vehicles[0]} />
-      <Spacecraft {...vehicles[1]} />
-      <Spacecraft {...vehicles[2]} />
+      <Spacecraft {...Vehicles[0]} />
+      <Spacecraft {...Vehicles[1]} />
+      <Spacecraft {...Vehicles[2]} />
       </div>
     </div>
   );
--- src/obj.js
+++ src/obj.js
@@ -1,10 +1,28 @@
-var vehicles = [
+var Vehicles = [
     {
-        height:'70 m',
-        diameter:'3.7 m',
-        mass:'549,054 kg',
-        payload:'22,800 kg'
+      name:'Falcon 9',
+      image:'falcon-9',
+      height:'70 m',
+      diameter:'3.7 m',
+      mass:'549,054 kg',
+      payload:'22,800 kg'
+    },
+    {
+      name:'Falcon heavy',
+      image:'falcon-heavy',
+      height:'70 m',
+      diameter:'3.7m',
+      mass:'1,420,788 Kg',
+      payload:'63,000 Kg',
+    },
+    {
+      name:'Starship SN15',
+      image:'starship',
+      height:'120 m',
+      diameter:'9 m',
+      mass:'',
+      payload:'100 t',
     }
-]
+  ]
 
 export default Vehicles
